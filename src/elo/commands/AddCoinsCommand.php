d43cb22c20cbbff0c0fc6bc811433418c89cdcdf
ZPlayzMCPE <jameshayward3800@gmail.com>
2017-07-10 19:49:47 +0100

    Update and rename AddEloCommand.php to AddCoinsCommand.php

--- src/elo/commands/AddCoinsCommand.php
+++ src/elo/commands/AddCoinsCommand.php
@@ -4,34 +4,34 @@ namespace elo\commands;
 
 use pocketmine\command\CommandSender;
 use pocketmine\command\PluginCommand;
-use elo\Elo;
+use elo\Coins;
 use pocketmine\utils\TextFormat as TF;
 use pocketmine\Player;
 
-class AddEloCommand extends PluginCommand {
+class AddCoinsCommand extends PluginCommand {
 
    private $main;
 
-	public function __construct(Elo $main, $name) {
+	public function __construct(Coins $main, $name) {
 		parent::__construct($name, $main);
 		$this->main = $main;
-		$this->setPermission("addelo.command");
+		$this->setPermission("addcoins.command");
 	}
 
 	public function execute(CommandSender $sender, $currentAlias, array $args) {
           if($this->testPermission($sender)){
 
            if(!isset($args[0])){
-             $sender->sendMessage(TF::RED."Usage: /addelo <player> <elo>");
+             $sender->sendMessage(TF::RED."Usage: /addcoins <player> <coins>");
               }
     
             if(isset($args[0])){
               $name = $args[0];
               if(isset($args[1])){
                  $lol = $args[1];
-                  $elo = (int)$lol;
-                   $this->main->addElo($name, $elo);
-                    $sender->sendMessage(Elo::prefix.TF::GREEN."Successfully added ".$elo." Elo to Player ".$name);
+                  $coins = (int)$lol;
+                   $this->main->addElo($name, $coins);
+                    $sender->sendMessage(Coins::prefix.TF::GREEN."Successfully added ".$coins." Coins to Player ".$name);
                 }
            }
        }
