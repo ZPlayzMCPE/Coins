aed6c80deaa39054459863b1b9252bc8f93be6cc
ZPlayzMCPE <jameshayward3800@gmail.com>
2017-07-18 14:57:09 +0100

    Update and rename AddCoinsCommand.php to AddBalCommand.php

--- src/elo/commands/AddBalCommand.php
+++ src/elo/commands/AddBalCommand.php
@@ -8,21 +8,21 @@ use elo\Coins;
 use pocketmine\utils\TextFormat as TF;
 use pocketmine\Player;
 
-class AddCoinsCommand extends PluginCommand {
+class AddBalCommand extends PluginCommand {
 
    private $main;
 
 	public function __construct(Coins $main, $name) {
 		parent::__construct($name, $main);
 		$this->main = $main;
-		$this->setPermission("addcoins.command");
+		$this->setPermission("addbal.command");
 	}
 
 	public function execute(CommandSender $sender, $currentAlias, array $args) {
           if($this->testPermission($sender)){
 
            if(!isset($args[0])){
-             $sender->sendMessage(TF::RED."Usage: /addcoins <player> <coins>");
+             $sender->sendMessage(TF::RED."Usage: /addbal <player> <balance>");
               }
     
             if(isset($args[0])){
@@ -30,8 +30,8 @@ class AddCoinsCommand extends PluginCommand {
               if(isset($args[1])){
                  $lol = $args[1];
                   $coins = (int)$lol;
-                   $this->main->addCoins($name, $coins);
-                    $sender->sendMessage(Coins::prefix.TF::GREEN."Successfully added ".$coins." Coins to Player ".$name);
+                   $this->main->addBal($name, $money);
+                    $sender->sendMessage(Coins::prefix.TF::GREEN."Successfully added ".$money." Balance to Player ".$name);
                 }
            }
        }
