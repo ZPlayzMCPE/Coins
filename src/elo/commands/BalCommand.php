cb6b1caef9a2dcdbd0f80a095dd87291230da8e3
ZPlayzMCPE <jameshayward3800@gmail.com>
2017-07-18 14:58:33 +0100

    Update and rename CoinsCommand.php to BalCommand.php

--- src/elo/commands/BalCommand.php
+++ src/elo/commands/BalCommand.php
@@ -4,27 +4,27 @@ namespace elo\commands;
 
 use pocketmine\command\CommandSender;
 use pocketmine\command\PluginCommand;
-use elo\Coins;
+use elo\Bal;
 use pocketmine\utils\TextFormat as TF;
 use pocketmine\Player;
 
-class CoinsCommand extends PluginCommand {
+class BalCommand extends PluginCommand {
 
  private $main;
 
 	public function __construct(Coins $main, $name) {
 		parent::__construct($name, $main);
 		$this->main = $main;
-		$this->setPermission("coins.command");
+		$this->setPermission("bal.command");
 	}
 
 	public function execute(CommandSender $sender, $currentAlias, array $args) {
           if($this->testPermission($sender)){
              if($sender instanceof Player){
              $coins = $this->main->getcoins($sender->getName());
-              $sender->sendMessage(Coins::prefix.TF::YELLOW."You have ".TF::BLUE.$coins.TF::YELLOW." Coins.");
+              $sender->sendMessage(Coins::prefix.TF::YELLOW."You have ".TF::BLUE.$money.TF::YELLOW." Balance.");
        }else{
-       $sender->sendMessage(TF::RED."You must run this command in game!");
+       $sender->sendMessage(TF::RED."You must run this command in game, silly!");
     }
   }
  }
